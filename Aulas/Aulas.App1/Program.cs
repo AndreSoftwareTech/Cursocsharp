d20de9716d3651c896d073a72804872bfe306519
Andre Granemann <andre@zuplae.com>
2023-04-09 03:17:37 -0300

    Subindo exemplo constantes

--- Aulas/Aulas.App1/Program.cs
+++ Aulas/Aulas.App1/Program.cs
@@ -10,21 +10,40 @@ namespace Aulas.App1
     {
         static void Main(string[] args)
         {
+
+            //tipos primitivos sting aspas duplas, char aspas simples
+            char flag = 'a';
             string nome = "Andre";
+            flag = 'b';
+
+            // duas variaveis do tipo int, diferenca esta na declaracao
             int idade = 25;
+            var idadenova = 25;
+
+            // booleano
+            bool verdadeiro = true;
+
+            // tipos primitivos flooat
             decimal valor = 100.99m;
             double valordois = 100.99;
             float valortrez = 100.99f;
-            var idadenova = 25;
 
+            //constante é dado atraves da palavra reservada const, nao permitindo reatribuir um novo valor posteriormente apos a criacao
+            const string descricao = "Curso Csharp"; 
 
+            
+            Console.WriteLine(flag);
             Console.WriteLine(nome);
+
             Console.WriteLine(idade);
+            Console.WriteLine(idadenova);
+
+            Console.WriteLine(verdadeiro);
+            
             Console.WriteLine(valor);
             Console.WriteLine(valordois);
             Console.WriteLine( valortrez);
-            Console.WriteLine(idadenova);
-
+            Console.WriteLine(descricao);
             Console.ReadLine();
         }
     }
