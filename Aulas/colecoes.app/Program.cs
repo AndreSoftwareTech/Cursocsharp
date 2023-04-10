9de4c2013c97a1555bd063ae3bd37bdde34b77ab
Andre Granemann <andre@zuplae.com>
2023-04-10 11:29:59 -0300

    Subindo estrutura array foreach

---
+++ Aulas/colecoes.app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+var arrayList = new ArrayList();
+
+arrayList.Add(1); //indice 0
+
+arrayList.Add("Andre");  //indice 1
+
+arrayList.Add(true); //indice 2
+
+//funcao interna para remover apenas um indice
+arrayList.RemoveAt(0);
+
+//funcao para limpar o Array List
+arrayList.Clear();
+
+//estrutura de repeticao para o indice do array List
+foreach (var item in arrayList)
+{
+    Console.WriteLine(item);
+}
+//Console.WriteLine(arrayList[2]);
---
+++ Aulas/funcional.app/Program.cs
@@ -0,0 +1,41 @@
+﻿// Trabalhando com funcoes
+
+// Variavel recebendo a funcao 
+var nome1 = NomeCompleto();
+
+var nome2 = NomeCompleto2();
+
+var soma = Soma(5, 5);
+
+
+
+
+
+
+
+//funcao com retorno de uma string
+string NomeCompleto()
+{
+    return "Andre Vitor Granemann Belli";
+}
+
+string NomeCompleto2()
+{
+    string nome = "Andre";
+    string sobrenome = "Granemann";
+    return nome + " " + sobrenome;
+}
+
+// funcao com parametros
+int Soma(int a, int b)
+
+{ 
+    return a + b;
+}
+
+
+
+Console.WriteLine(nome1);
+Console.WriteLine(nome2);
+Console.WriteLine(soma);
+
